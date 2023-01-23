993477cfc3add034090e03744d9e4b77b6f6cb68
Tariqazan <45086258+Tariqazan@users.noreply.github.com>
2023-01-23 19:55:15 +0600

    python support 3.11

--- summarizing_product_details.py
+++ summarizing_product_details.py
@@ -1,11 +1,12 @@
+import heapq
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from transformers import pipeline
-
+import nltk
+from nltk.corpus import stopwords
 
 # print('*'*100)
 # url = input("Enter target website url: ")
@@ -39,9 +40,37 @@ def extract_relevant_information(url):
 extract_relevant_information = extract_relevant_information(url)
 print(extract_relevant_information)
 
-summarization = pipeline("summarization")
-summary_text = summarization(extract_relevant_information)[0]['summary_text']
+
+sentence_list = nltk.sent_tokenize(extract_relevant_information)
+
+stopwords = nltk.corpus.stopwords.words('english')
+
+word_frequencies = {}
+for word in nltk.word_tokenize(extract_relevant_information):
+    if word not in stopwords:
+        if word not in word_frequencies.keys():
+            word_frequencies[word] = 1
+        else:
+            word_frequencies[word] += 1
+
+maximum_frequncy = max(word_frequencies.values())
+
+for word in word_frequencies.keys():
+    word_frequencies[word] = (word_frequencies[word]/maximum_frequncy)
+sentence_scores = {}
+for sent in sentence_list:
+    for word in nltk.word_tokenize(sent.lower()):
+        if word in word_frequencies.keys():
+            if len(sent.split(' ')) < 30:
+                if sent not in sentence_scores.keys():
+                    sentence_scores[sent] = word_frequencies[word]
+                else:
+                    sentence_scores[sent] += word_frequencies[word]
+
+summary_sentences = heapq.nlargest(7, sentence_scores, key=sentence_scores.get)
+
+summary = ' '.join(summary_sentences)
 
 print("="*50)
-print("Summary:", summary_text)
+print("Summary:", summary)
 print("="*50)
